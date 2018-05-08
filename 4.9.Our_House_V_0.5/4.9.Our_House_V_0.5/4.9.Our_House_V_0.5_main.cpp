79ce960c7c3e255ed05ff3f198d42fd6faa992e7
강현구 <brusque4owl@gmail.com>
2018-05-08 18:29:28 +0900

    CAMERA_SETTING_TRANSLATION 180508_1731 : Implement motion_translate_vaxis() function.

--- 4.9.Our_House_V_0.5/4.9.Our_House_V_0.5/4.9.Our_House_V_0.5_main.cpp
+++ 4.9.Our_House_V_0.5/4.9.Our_House_V_0.5/4.9.Our_House_V_0.5_main.cpp
@@ -270,6 +270,7 @@ void mousepress(int button, int state, int x, int y) {
 }
 
 #define CAM_ROT_SENSITIVITY 0.15f
+#define CAM_TRA_SENSITIVITY 1.0f
 void motion_1(int x, int y) {
 	glm::mat4 mat4_tmp;
 	glm::vec3 vec3_tmp;
@@ -306,7 +307,26 @@ void motion_translate_uaxis(int x, int y) {
 	glutPostRedisplay();
 }
 void motion_translate_vaxis(int x, int y) {
-	glutPostRedisplay();
+	glm::mat4 mat4_tmp;
+	glm::vec3 vec3_tmp;
+	float dely;
+
+	if (leftbutton_pressed) {
+		dely = -(float)(y - prevy);
+		prevy = y;
+
+		mat4_tmp = glm::translate(glm::mat4(1.0f), dely*camera[camera_selected].vup);
+		
+		// prp(카메라 위치), vrp(카메라가 바라보는 곳)은 변경  // vup(카메라 위)은 불변 - 회전하는 것이 아니기 때문
+		camera[camera_selected].prp = glm::vec3(mat4_tmp*glm::vec4(camera[camera_selected].prp, 1.0f));	// affine transformation of point (x,y,z,1)
+		camera[camera_selected].vrp = glm::vec3(mat4_tmp*glm::vec4(camera[camera_selected].vrp, 1.0f));	// affine transformation of point (x,y,z,1)
+		//camera[camera_selected].vup = glm::vec3(mat4_tmp*glm::vec4(camera[camera_selected].vup, 0.0f));	// affine transformation of vector(x,y,z,0)
+
+		ViewMatrix[camera_selected] = glm::lookAt(camera[camera_selected].prp, camera[camera_selected].vrp, camera[camera_selected].vup);
+
+		ViewProjectionMatrix[camera_selected] = ProjectionMatrix[camera_selected] * ViewMatrix[camera_selected];
+		glutPostRedisplay();
+	}
 }
 void motion_translate_naxis(int x, int y) {
 	glutPostRedisplay();
